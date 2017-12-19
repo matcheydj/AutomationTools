014f6592d7476191ba70716871c36272bdf932d8
wangzhaodong8 <34636361+wangzhaodong8@users.noreply.github.com>
2017-12-19 13:50:29 +0800

    Remove password

--- bash/Mount.sh
+++ bash/Mount.sh
@@ -3,7 +3,7 @@ if [ $? -ne 0 ];then
    echo "/Volumes/Resources   auto_resources" >>/etc/auto_master
 fi
 if [ ! -e "/etc/auto_resources" ];then
-    echo "content -fstype=smbfs   ://content:!LANDesk123@172.29.150.129/content"   >/etc/auto_resources
+    echo "content -fstype=smbfs   ://content:password@172.29.150.129/content"   >/etc/auto_resources
     echo "vulnerabilities -fstype=smbfs   ://content:!LANDesk123@172.29.150.129/vulnerabilities" >>/etc/auto_resources
 fi
 automount -vc
